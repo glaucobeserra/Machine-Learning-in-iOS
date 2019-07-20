9bcc25411412939d4dc58433d71b1ba92ce08c52
Glauco Dantas Beserra <dantas.glauco@gmail.com>
2019-07-20 20:45:22 -0300

    Vision
    Apple's Vision framework was introduced along with Core ML in iOS 11. They integrate easily for computer vision tasks.
    You can now run the app and try to classify images with the device.

--- Project #1 - HealthySnacks/HealthySnacks/Snacks/ViewController.swift
+++ Project #1 - HealthySnacks/HealthySnacks/Snacks/ViewController.swift
@@ -1,84 +1,120 @@
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
-  
-  @IBOutlet var imageView: UIImageView!
-  @IBOutlet var cameraButton: UIButton!
-  @IBOutlet var photoLibraryButton: UIButton!
-  @IBOutlet var resultsView: UIView!
-  @IBOutlet var resultsLabel: UILabel!
-  @IBOutlet var resultsConstraint: NSLayoutConstraint!
-
-  var firstTime = true
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
-    resultsView.alpha = 0
-    resultsLabel.text = "choose or take a photo"
-  }
-
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-
-    // Show the "choose or take a photo" hint when the app is opened.
-    if firstTime {
-      showResultsView(delay: 0.5)
-      firstTime = false
-    }
-  }
-  
-  @IBAction func takePicture() {
-    presentPhotoPicker(sourceType: .camera)
-  }
-
-  @IBAction func choosePhoto() {
-    presentPhotoPicker(sourceType: .photoLibrary)
-  }
-
-  func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
-    let picker = UIImagePickerController()
-    picker.delegate = self
-    picker.sourceType = sourceType
-    present(picker, animated: true)
-    hideResultsView()
-  }
-
-  func showResultsView(delay: TimeInterval = 0.1) {
-    resultsConstraint.constant = 100
-    view.layoutIfNeeded()
-
-    UIView.animate(withDuration: 0.5,
-                   delay: delay,
-                   usingSpringWithDamping: 0.6,
-                   initialSpringVelocity: 0.6,
-                   options: .beginFromCurrentState,
-                   animations: {
-      self.resultsView.alpha = 1
-      self.resultsConstraint.constant = -10
-      self.view.layoutIfNeeded()
-    },
-    completion: nil)
-  }
-
-  func hideResultsView() {
-    UIView.animate(withDuration: 0.3) {
-      self.resultsView.alpha = 0
-    }
-  }
-
-  func classify(image: UIImage) {
     
-  }
+    // MARK: - Outlets
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var cameraButton: UIButton!
+    @IBOutlet var photoLibraryButton: UIButton!
+    @IBOutlet var resultsView: UIView!
+    @IBOutlet var resultsLabel: UILabel!
+    @IBOutlet var resultsConstraint: NSLayoutConstraint!
+    
+    
+    // MARK: - Properties
+    var firstTime = true
+    
+    lazy var classificationRequest: VNCoreMLRequest = {
+        let visionModel = try! VNCoreMLModel(for: HealthySnacks().model)
+        let request = VNCoreMLRequest(model: visionModel) { [unowned self] request, _ in
+            self.processObservations(for: request)
+        }
+        request.imageCropAndScaleOption = .centerCrop
+        return request
+    }()
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        resultsView.alpha = 0
+        resultsLabel.text = "choose or take a photo"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Show the "choose or take a photo" hint when the app is opened.
+        if firstTime {
+            showResultsView(delay: 0.5)
+            firstTime = false
+        }
+    }
+    
+    // MARK: - Actions
+    @IBAction func takePicture() {
+        presentPhotoPicker(sourceType: .camera)
+    }
+    
+    @IBAction func choosePhoto() {
+        presentPhotoPicker(sourceType: .photoLibrary)
+    }
+    
+    // MARK: - Methods
+    func presentPhotoPicker(sourceType: UIImagePickerController.SourceType) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = sourceType
+        present(picker, animated: true)
+        hideResultsView()
+    }
+    
+    func showResultsView(delay: TimeInterval = 0.1) {
+        resultsConstraint.constant = 100
+        view.layoutIfNeeded()
+        
+        UIView.animate(withDuration: 0.5,
+                       delay: delay,
+                       usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 0.6,
+                       options: .beginFromCurrentState,
+                       animations: {
+                        self.resultsView.alpha = 1
+                        self.resultsConstraint.constant = -10
+                        self.view.layoutIfNeeded()
+        },
+                       completion: nil)
+    }
+    
+    func hideResultsView() {
+        UIView.animate(withDuration: 0.3) {
+            self.resultsView.alpha = 0
+        }
+    }
+    
+    func classify(image: UIImage) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            guard let ciImage = CIImage(image: image) else { return }
+            let orientation = CGImagePropertyOrientation(image.imageOrientation)
+            let handler = VNImageRequestHandler(ciImage: ciImage,
+                                                orientation: orientation)
+            try? handler.perform([self.classificationRequest])
+        }
+    }
+    
+    func processObservations(for request: VNRequest) {
+        DispatchQueue.main.async {
+            guard let result = request.results?.first as? VNClassificationObservation else { return }
+            let formatter = NumberFormatter()
+            formatter.maximumFractionDigits = 1
+            let calculatedResult = result.confidence * 100
+            guard let confidencePercentage = formatter.string(from: NSNumber(value: calculatedResult)) else { return }
+            self.resultsLabel.text = "\(result.identifier) \(confidencePercentage)%"
+            self.showResultsView()
+        }
+    }
 }
 
+// MARK: - UIimagePickerControllerDelegate and UINavigationControllerDelegate extensions
 extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-    picker.dismiss(animated: true)
-
-	let image = info[.originalImage] as! UIImage
-    imageView.image = image
-
-    classify(image: image)
-  }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        
+        let image = info[.originalImage] as! UIImage
+        imageView.image = image
+        
+        classify(image: image)
+    }
 }
